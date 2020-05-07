acd79945b090335d3ec6c4c4d901b267360a7957
Agata Kuska <kawabezcukru@onet.eu>
2020-05-07 15:47:23 +0200

    Add scroll feature

--- scroller-dots/main.js
+++ scroller-dots/main.js
@@ -1,12 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('Załadowało');
+ //   console.log('Załadowało');
 
     const rootElement = document.querySelector('#root');
     const sections = document.querySelectorAll('section');
+    let currentSectionIndex = 0;
+    let isThrottled = false;
 
     document.addEventListener('mousewheel', (e) => {
-        console.log(e.wheelDelta);
-        
+        if (isThrottled) return;
+        isThrottled = true;
+
+        setTimeout( () => {
+            isThrottled = false;
+        }, 1000);
+
+        const direction = e.wheelDelta < 0 ? 1 : -1;
+
+        if (direction === 1) {
+            const isLastSection = currentSectionIndex === sections.length - 1;
+            if (isLastSection) return;
+          } else if(direction === -1) {
+            const firstSection = currentSectionIndex === 0;
+            if (firstSection) return;
+          }
+        currentSectionIndex = currentSectionIndex + direction
+       // console.log(currentSectionIndex);
+        sections[currentSectionIndex].scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+        })
     })
     
     
