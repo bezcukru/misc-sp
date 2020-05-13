ace0b02b338f112082ce60261b623f39a44299d3
Agata Kuska <kawabezcukru@onet.eu>
2020-05-13 18:11:21 +0200

    Add swiper for touch screens

--- scroller-dots/main.js
+++ scroller-dots/main.js
@@ -2,5 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const scroller = new Scroller('#root');
 
     document.addEventListener('wheel', (event) => scroller.listenScroll(event));
+    document.addEventListener('swipeUp', () => scroller.scroll(1));
+    document.addEventListener('swipeDown', () => scroller.scroll(-1));
 
 });
