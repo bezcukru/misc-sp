1cd5ba1aa1058cd7ca5771f0662a352d39422306
Agata Kuska <kawabezcukru@onet.eu>
2020-05-13 12:17:33 +0200

    Add Scroller class functions

--- scroller-dots/Scroller.js
+++ scroller-dots/Scroller.js
@@ -1,8 +1,67 @@
 class Scroller {
-    constructor() {
-        const rootElement = document.querySelector('#root');
-        const sections = document.querySelectorAll('section');
-        let currentSectionIndex = 0;
-        let isThrottled = false;
+    constructor(rootSelector) {
+        const rootElement = document.querySelector(rootSelector);
+        this.sections = document.querySelectorAll('section');
+        this.currentSectionIndex = 0;
+        this.isThrottled = false;
+
+        this.listenScroll = this.listenScroll.bind(this);
+        this.scroll = this.scroll.bind(this);
+        this.scrollToCurrentSection = this.scrollToCurrentSection.bind(this);
+
+        this.isScrolledIntoView(this.sections[0]);
+    }
+
+    isScrolledIntoView(el) {
+        const rect = el.getBoundingClientRect();
+        const elemTop = rect.top;
+        const elemBottom = Math.floor(rect.bottom);
+        
+        const isVisible = (elemTop>= 0) && (elemBottom <= window.innerHeight);
+
+        console.log(isVisible);
+        
+
+        }
+
+    listenScroll(e) {
+        if (this.isThrottled) return;
+        this.isThrottled = true;
+        setTimeout(() => {
+            this.isThrottled = false;
+        }, 1000);
+
+        const direction = e.wheelDelta < 0 ? 1 : -1;
+
+
+
+        this.scroll(direction);
+    }
+
+    scroll(direction) {
+
+        if (direction === 1) {
+            const isLastSection = this.currentSectionIndex === this.sections.length - 1;
+            if (isLastSection) return;
+
+        } else if (direction === -1) {
+
+            const firstSection = this.currentSectionIndex === 0;
+            if (firstSection) return;
+
+        }
+
+        this.currentSectionIndex = this.currentSectionIndex + direction;
+
+        this.scrollToCurrentSection();
+
+    }
+
+    scrollToCurrentSection() {
+
+        this.sections[this.currentSectionIndex].scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+        })
     }
 }
--- scroller-dots/main.js
+++ scroller-dots/main.js
@@ -1,43 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const scroller = new Scroller('#root');
 
-    const rootElement = document.querySelector('#root');
-    const sections = document.querySelectorAll('section');
-    let currentSectionIndex = 0;
-    let isThrottled = false;
+    document.addEventListener('mousewheel', scroller.listenScroll);
 
-    document.addEventListener('mousewheel', (e) => {
-        if (isThrottled) return;
-        isThrottled = true;
-
-        setTimeout(() => {
-            isThrottled = false;
-        }, 1000);
-
-        const direction = e.wheelDelta < 0 ? 1 : -1;
-
-        scroll(direction);
-
-    })
-
-    const scroll = (direction) => {
-        if (direction === 1) {
-            const isLastSection = currentSectionIndex === sections.length - 1;
-            if (isLastSection) return;
-        } else if (direction === -1) {
-            const firstSection = currentSectionIndex === 0;
-            if (firstSection) return;
-        }
-
-        currentSectionIndex = currentSectionIndex + direction
-
-        scrollToCurrentSection();
-
-    }
-
-    const scrollToCurrentSection = () => {
-        sections[currentSectionIndex].scrollIntoView({
-            behavior: "smooth",
-            block: "start",
-        })
-    }
-})
+});
