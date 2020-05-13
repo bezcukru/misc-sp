edbd4bf98791100f1bf20dd5a1b2e1d7081c448c
Agata Kuska <kawabezcukru@onet.eu>
2020-05-13 18:22:44 +0200

    Add support for keyboard

--- scroller-dots/main.js
+++ scroller-dots/main.js
@@ -4,5 +4,14 @@ document.addEventListener('DOMContentLoaded', () => {
     document.addEventListener('wheel', (event) => scroller.listenScroll(event));
     document.addEventListener('swipeUp', () => scroller.scroll(1));
     document.addEventListener('swipeDown', () => scroller.scroll(-1));
-
+    document.addEventListener('keydown', (event) => {
+        switch (event.keyCode) {
+            case 40:
+                return scroller.scroll(1);
+            case 38:
+                return scroller.scroll(-1);
+            default:
+                return;
+        }
+    })
 });
