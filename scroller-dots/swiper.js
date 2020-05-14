300e78dd62f6896d08b6330ca62a1d167abc4719
Agata Kuska <kawabezcukru@onet.eu>
2020-05-14 21:38:44 +0200

    before merge

--- scroller-dots/swiper.js
+++ scroller-dots/swiper.js
@@ -1,17 +1,17 @@
 class Swiper {
     constructor() {
-    this.initialY = null;
-    this.initialX = null;
+        this.initialY = null;
+        this.initialX = null;
 
-    document.addEventListener('touchstart', (event) => this.startTouch(event));
-    document.addEventListener('touchmove', (event) => this.moveTouch(event));
+        document.addEventListener('touchstart', (event) => this.startTouch(event));
+        document.addEventListener('touchmove', (event) => this.moveTouch(event));
 
-    this.events = {
-        swipeUp: new Event('swipeUp'),
-        swipeDown: new Event('swipeDown'),
-        swipeLeft: new Event('swipeLeft'),
-        swipeRight: new Event('swipeRight'),
-    }
+        this.events = {
+            swipeUp: new Event('swipeUp'),
+            swipeDown: new Event('swipeDown'),
+            swipeLeft: new Event('swipeLeft'),
+            swipeRight: new Event('swipeRight'),
+        }
     }
 
     startTouch(event) {
@@ -21,7 +21,7 @@ class Swiper {
     }
 
     moveTouch(event) {
-        if(!this.initialX || !this.initialY) return;
+        if (!this.initialX || !this.initialY) return;
 
         const currentX = event.touches[0].clientX;
         const currentY = event.touches[0].clientY;
@@ -30,11 +30,11 @@ class Swiper {
         const diffY = this.initialY - currentY;
 
         if (Math.abs(diffX) > Math.abs(diffY)) {
-           if (diffX > 0) {
-               document.dispatchEvent(this.events.swipeLeft);
-           } else {
-            document.dispatchEvent(this.events.swipeRight);
-           }
+            if (diffX > 0) {
+                document.dispatchEvent(this.events.swipeLeft);
+            } else {
+                document.dispatchEvent(this.events.swipeRight);
+            }
         } else {
             // jesteśmy w osi Y
             if (diffY > 0) {
@@ -42,7 +42,7 @@ class Swiper {
             } else {
                 document.dispatchEvent(this.events.swipeDown);
             }
-     
+
         }
         this.initialX = null;
         this.initialY = null;
